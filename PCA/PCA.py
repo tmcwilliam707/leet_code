910d0cd4af324777269a7fcdf00e6be8949dc1b7
tSpatial <71474441+tcmwilliam707@users.noreply.github.com>
2024-01-27 23:11:42 -0800

    PCA
    PCA on image on cyber truck, just playing with PCA library from sci-kit learn performing PCA on images

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -1,13 +1,10 @@
+import os
 import numpy as np
 from PIL import Image
-import requests
-from io import BytesIO
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-url = "PCA/Image.jpg"
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
+img = Image.open("Cyber.jpg")
 
 # Convert the image to grayscale
 img_gray = img.convert('L')
@@ -15,13 +12,14 @@ img_gray = img.convert('L')
 # Convert the image to a numpy array
 img_array = np.array(img_gray)
 
-# Flatten the image array and perform PCA
-img_flattened = img_array.flatten().reshape(1, -1)
-pca = PCA(n_components=200)
-pca.fit(img_flattened)
+# Treat each row of pixels as a separate sample
+img_reshaped = img_array.reshape(img_array.shape[0], -1)
+
+pca = PCA(n_components=700)
+pca.fit(img_reshaped)
 
 # Use the principal components to reconstruct the image
-components = pca.transform(img_flattened)
+components = pca.transform(img_reshaped)
 projected = pca.inverse_transform(components)
 
 # Reshape the reconstructed image to its original dimensions
